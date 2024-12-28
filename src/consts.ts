c064b27d25066683d900c30fbc7500f034c3de8b
Gitudon <oniku.udon.029@gmail.com>
2024-12-28 11:12:14 +0900

    astroで仮実装

---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = '立川高校 受験体験記';
+export const SITE_DESCRIPTION = '立川高校の卒業生による受験体験記のサイトです。';
+export const GITHUB_URL = 'https://github.com/https://github.com/Tckw-Jukentaikenki';
+export const TEST_IMG = '/blog-placeholder-2.jpg'
