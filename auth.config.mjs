3cb9d2218f4ac6ce20a3f9bc02275fc8c582a40b
Gitudon <oniku.udon.029@gmail.com>
2025-02-21 22:56:59 +0900

    記事の見出し統一+認証の初期設定

--- auth.config.mjs
+++ auth.config.mjs
@@ -1,14 +1,11 @@
 import GitHub from '@auth/core/providers/github';
 import { defineConfig } from 'auth-astro';
-import vercel from '@astrojs/vercel-adapter';
 
 export default defineConfig({
-    // providers: [
-    //     GitHub({
-    //         clientId: import.meta.env.GITHUB_CLIENT_ID,
-    //         clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
-    //     }),
-    // ],
-    output: 'server',
-    adapter: vercel(),
+    providers: [
+        GitHub({
+            clientId: import.meta.env.GITHUB_CLIENT_ID,
+            clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
+        }),
+    ],
 });
