544ff4a73b2ab709c8baa3c6d336f3a20a2d39a9
Gitudon <oniku.udon.029@gmail.com>
2025-02-21 23:12:19 +0900

    ？

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import auth from 'auth-astro';
-
 import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://tckw-jukentaikenki.github.io',
-    // integrations: [mdx(), sitemap(), auth()],
     integrations: [mdx(), sitemap()],
-    adapter: node({
-        mode: 'standalone',
-    }),
+    // integrations: [mdx(), sitemap(), auth()],
+    // adapter: node({
+    //     mode: 'standalone',
+    // }),
 });
